814461ba899ee62c78dbb578585e149a8a173c75
Davidm8624 <davidm8624@gmail.com>
2023-07-24 14:17:02 -0700

    db project

---
+++ db.rb
@@ -0,0 +1,11 @@
+#creates a way to add adn retrieve data from a db. the info will be stored in a diffrent location. For now it is on the same page for simplicity
+
+db = []
+
+action = gets
+
+case action{
+when look_up{
+  
+}
+}
