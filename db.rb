e961068fb15d9a6b9184b9bfdd0520d783c22a98
Davidm8624 <davidm8624@gmail.com>
2023-08-20 15:14:44 -0700

    fix stupid case statment when not as sleepy

--- db.rb
+++ db.rb
@@ -4,16 +4,16 @@ def db = []
 
 def action = gets
 
-case (action){
-when look_up{
+case (action) when
+when look_up when
   def look_for = gets
-  if db.includes(look_for){
+  if db.includes(look_for) do
     puts "#{look_for} is in the system"
-  }else{
+  else do
     puts "that person is not in the system, please check your spelling and start agian"
-  }
-}
-when add_person{
+  end
+end
+when add_person do
   puts "who would you like to add?"
   def name = gets
   puts "#{name}, is that correct? Y/N"
@@ -25,17 +25,17 @@ when add_person{
   #   name = gets
   #}
 
-  until is_ok === "y" || "Y"{
+  until is_ok === "y" || "Y" do
     puts "re-eter the name"
     name = gets
     puts "#{name}, is that correct? Y/N"
     is_ok = gets
     puts "we made it to here"
-  }else{
+  else do
     db.push(name)
     puts "#{name} has been added to db"
-  }
-}
-}
+  end
+end
+end
 
 #im stupid i did this all js style. remake this with "end" rather then "}"
